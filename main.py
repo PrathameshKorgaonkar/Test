a06d30d641cc1157fb6d29d79a64a6789ddf13d5
Prathamesh Prakash Korgaonkar <141243426+PrathameshKorgaonkar@users.noreply.github.com>
2024-06-09 15:06:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('build_number')
     parser.add_argument('--jobs')
     args = parser.parse_args()
     jobs = args.jobs.split(',')
+    print(f'Build Number:{args.build_number}')
     print(f'Job Count:{len(jobs)}\nJob Name:{jobs}')
 
 if __name__ == "__main__":
