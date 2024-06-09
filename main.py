e361686fd344bc7adb33d5c7bf02e44e6e080c86
Prathamesh Prakash Korgaonkar <141243426+PrathameshKorgaonkar@users.noreply.github.com>
2024-06-09 16:08:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('url')
+    parser.add_argument('username')
+    parser.add_argument('password')
     parser.add_argument('build_number')
     parser.add_argument('--jobs')
     args = parser.parse_args()
     jobs = args.jobs.split(',')
+    print(f'Url:{args.url}')
+    print(f'Username:{args.username}')
+    print(f'Password:{args.password}')
     print(f'Build Number:{args.build_number}')
     print(f'Job Count:{len(jobs)}\nJob Name:{jobs}')
 
