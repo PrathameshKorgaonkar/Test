00acc9d3068e17c7c09ac79203f74d377f19cdb2
Prathamesh Prakash Korgaonkar <141243426+PrathameshKorgaonkar@users.noreply.github.com>
2024-06-09 14:28:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--jobs')
     args = parser.parse_args()
-    jobs = args.choices.split(',')
+    jobs = args.jobs.split(',')
     print(f'Job Count:{len(jobs)}\nJob Name:{jobs}')
 
 if __name__ == "__main__":
